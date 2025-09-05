e84a42ced6cc02635dec883152c74f425d25edc3
Tom <2508893587@qq.com>
2025-09-06 07:51:20 +0800

    test commit

--- astro.config.ts
+++ astro.config.ts
@@ -13,7 +13,7 @@ import { SITE } from "./src/config";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 
-// https://astro.build/config11
+// https://astro.build/config
 export default defineConfig({
   site: SITE.website,
   integrations: [
