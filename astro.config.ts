1e14ed38e61240b5953668df927b00979c02fef8
Tom <2508893587@qq.com>
2025-09-06 07:22:44 +0800

    test commit

--- astro.config.ts
+++ astro.config.ts
@@ -15,8 +15,6 @@ import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
-  site: ' https://tangbinqiang.github.io/AstroBlog-TomCodeHub/',
-  base: '/AstroBlog-TomCodeHub',
   integrations: [
     sitemap({
       filter: page => SITE.showArchives || !page.endsWith("/archives"),
